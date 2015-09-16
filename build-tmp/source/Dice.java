8ba33a9af4737adf0606cfc812ec1baf8a81dd67
unknown <jitan16@697-334-Nogrod.sf.k12.internal>
2015-09-16 10:40:57 -0700

    new updates

--- build-tmp/source/Dice.java
+++ build-tmp/source/Dice.java
@@ -1,10 +1,26 @@
-void setup()
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class Dice extends PApplet {
+
+public void setup()
 {
     noLoop();
     size(500,500);
 
 }
-void draw()
+public void draw()
 {	background(255);
   	int total=0;
    	for (int x=0; x<=500; x=x+50)
@@ -24,7 +40,7 @@ void draw()
 		text("There are"+ total,200,490);
 
 }
-void mousePressed()
+public void mousePressed()
 {
     redraw();
 }
@@ -39,7 +55,7 @@ class Die //models one single dice cube
 
     }
     int value=(int)(Math.random()*6)+1;
-    void roll()
+    public void roll()
     {
     	
     	
@@ -89,7 +105,7 @@ class Die //models one single dice cube
     	}
       	
     }
-    void show()
+    public void show()
     {
 		roll();
         rect(myX,myY,50,50);
@@ -139,4 +155,13 @@ class Die //models one single dice cube
     }
     	
     
+}
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "Dice" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
 }
